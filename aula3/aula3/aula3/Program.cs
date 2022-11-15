2dad3f86f8fb29c64c9245c63ffb76c7db3174c4
Joao Teixeira <92153307+joaotx6@users.noreply.github.com>
2022-11-15 18:53:04 +0000

    aula 3 teste rep

---
+++ aula3/aula3/aula3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace aula3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string csharp = "c#";
+        string dotnet = ".NET";
+        string csharpDotNet = csharp + dotnet;
+        Console.WriteLine(csharpDotNet);
+
+        
+
+
+
+    }
+}
+
